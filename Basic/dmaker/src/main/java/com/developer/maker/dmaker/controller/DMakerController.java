6ee8070e841bca14cdda4852dee0b6a848a40994
hyeon <vikim1210@naver.com>
2023-08-10 00:57:34 +0900

    [Feat] DMakerController: DMakerService의 createDeveloper 테스트(post가 맞으나 get으로 간단하게 진행)

--- Basic/dmaker/src/main/java/com/developer/maker/dmaker/controller/DMakerController.java
+++ Basic/dmaker/src/main/java/com/developer/maker/dmaker/controller/DMakerController.java
@@ -2,6 +2,7 @@ package com.developer.maker.dmaker.controller;
 
 import com.developer.maker.dmaker.service.DMakerService;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -20,6 +21,16 @@ public class DMakerController {
         log.info("GET /developers HTTP/1.1");
 
         return Arrays.asList("snow", "Elsa", "Anna");
+    }
+
+    //Get은 데이터를 받아오는 것으로 지금처럼 생성은 post가 맞음
+    @GetMapping("/create-developers")
+    public List<String> createDevelopers(){
+        log.info("GET /create-developers HTTP/1.1");
+
+        dMakerService.createDeveloper();
+
+        return Collections.singletonList("Barbie");
     }
 
 }
