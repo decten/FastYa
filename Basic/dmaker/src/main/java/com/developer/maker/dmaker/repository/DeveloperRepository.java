ddf79488a90d231be1414ca5ca86f81cbe40fb0f
hyeon <vikim1210@naver.com>
2023-08-10 00:24:48 +0900

    [Add&Feat] DeveloperRepository: JpaRepository를 사용한 인터페이스 구현

---
+++ Basic/dmaker/src/main/java/com/developer/maker/dmaker/repository/DeveloperRepository.java
@@ -0,0 +1,10 @@
+package com.developer.maker.dmaker.repository;
+
+import com.developer.maker.dmaker.entity.Developer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DeveloperRepository extends JpaRepository<Developer, Long> {
+
+}
