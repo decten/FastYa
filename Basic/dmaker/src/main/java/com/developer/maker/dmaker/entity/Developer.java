51d0739f0d4661b87d1e17b314a3b06ea430f6f9
hyeon <vikim1210@naver.com>
2023-08-09 22:42:43 +0900

    [Add] DTO: JPA에 맞게 Developer 작성

---
+++ Basic/dmaker/src/main/java/com/developer/maker/dmaker/entity/Developer.java
@@ -0,0 +1,29 @@
+package com.developer.maker.dmaker.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Developer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    protected Long id;
+
+    @Enumerated(EnumType.STRING)
+    private DeveloperLevel developerLevel;
+}
---
+++ Basic/dmaker/src/main/java/com/developer/maker/dmaker/entity/DeveloperLevel.java
@@ -0,0 +1,4 @@
+package com.developer.maker.dmaker.entity;
+
+public enum DeveloperLevel {
+}
