95a8dd3e16b7c75c800f591440223d4767a2a2aa
hyeon <vikim1210@naver.com>
2023-08-10 00:52:59 +0900

    [Feat] DMakerController: 빈으로 등록 된 DMakerService에 생성자를 만들어서 사용 함(@RequiredArgsConstructor)

--- Basic/dmaker/src/main/java/com/developer/maker/dmaker/controller/DMakerController.java
+++ Basic/dmaker/src/main/java/com/developer/maker/dmaker/controller/DMakerController.java
@@ -1,14 +1,18 @@
 package com.developer.maker.dmaker.controller;
 
+import com.developer.maker.dmaker.service.DMakerService;
 import java.util.Arrays;
 import java.util.List;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
+@RequiredArgsConstructor
 public class DMakerController {
+    private final DMakerService dMakerService;
 
     //GET /developers 요청 처리
     @GetMapping("/developers")
