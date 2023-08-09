0e3214f202df8e96bbbe0870dbdbd4e6046a7733
hyeon <vikim1210@naver.com>
2023-08-10 00:48:17 +0900

    [Feat] DMakerService: 엔티티 생성 후 DB에 저장하는 createDeveloper 함수 완성

--- Basic/dmaker/src/main/java/com/developer/maker/dmaker/service/DMakerService.java
+++ Basic/dmaker/src/main/java/com/developer/maker/dmaker/service/DMakerService.java
@@ -1,6 +1,10 @@
 package com.developer.maker.dmaker.service;
 
+import com.developer.maker.dmaker.entity.Developer;
 import com.developer.maker.dmaker.repository.DeveloperRepository;
+import com.developer.maker.dmaker.type.DeveloperLevel;
+import com.developer.maker.dmaker.type.DeveloperSkillType;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -8,4 +12,17 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class DMakerService {
     private final DeveloperRepository developerRepository;
+
+    @Transactional
+    public void createDeveloper(){
+        Developer developer = Developer.builder()
+            .developerLevel(DeveloperLevel.JUNIOR)
+            .developerSkillType(DeveloperSkillType.BACK_END)
+            .experienceYears(2)
+            .name("Barbie")
+            .age(27)
+            .build();
+
+        developerRepository.save(developer);
+    }
 }
