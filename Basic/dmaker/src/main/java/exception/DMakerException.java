fc120740bb8e98c9f534a230941e18251f14de3c
hyeon <vikim1210@naver.com>
2023-08-12 01:38:25 +0900

    [Add&Feat] DMakerErrorCode, DMakerException: 커스텀 에러 메시지 클래스 생성

---
+++ Basic/dmaker/src/main/java/exception/DMakerException.java
@@ -0,0 +1,20 @@
+package exception;
+
+import lombok.Getter;
+
+@Getter
+public class DMakerException extends RuntimeException{
+    private DMakerErrorCode dMakerErrorCode;
+    private String detailMessage;
+
+    public DMakerException(DMakerErrorCode errorCode){
+        super(errorCode.getMessage());
+        this.dMakerErrorCode = errorCode;
+        this.detailMessage = errorCode.getMessage();
+    }
+
+    public DMakerException(DMakerErrorCode errorCode, String detailMessage){
+        this.dMakerErrorCode = errorCode;
+        this.detailMessage = detailMessage;
+    }
+}
