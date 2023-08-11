40340eb9b06ec2dc4bf7d2a7df75a19755a8c4f1
hyeon <vikim1210@naver.com>
2023-08-11 23:40:58 +0900

    [Add&Feat] CreateDeveloper: Developer 데이터 밸리데이션 생성 및 완성

---
+++ Basic/dmaker/src/main/java/com/developer/maker/dmaker/dto/CreateDeveloper.java
@@ -0,0 +1,59 @@
+package com.developer.maker.dmaker.dto;
+
+import com.developer.maker.dmaker.type.DeveloperLevel;
+import com.developer.maker.dmaker.type.DeveloperSkillType;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.antlr.v4.runtime.misc.NotNull;
+
+public class CreateDeveloper {
+    @Getter
+    @Setter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Builder
+    public static class Request{
+        @NotNull
+        private DeveloperLevel developerLevel;
+
+        @NotNull
+        private DeveloperSkillType developerSkillType;
+
+        @NotNull
+        @Min(0)
+        @Max(20)
+        private Integer experienceYears;
+
+        @NotNull
+        @Size(min = 3, max = 50, message = "memberId size must 3~50")
+        private String memberId;
+
+        @NotNull
+        @Size(min = 3, max = 20, message = "name size must 3~20")
+        private String name;
+
+        @Min(18)
+        private Integer age;
+    }
+
+    @Getter
+    @Setter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Builder
+    public static class Response{
+        private DeveloperLevel developerLevel;
+        private DeveloperSkillType developerSkillType;
+        private Integer experienceYears;
+
+        private String memberId;
+        private String name;
+        private Integer age;
+    }
+}
