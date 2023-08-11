91bf59bdea0ff77c640410ae36e1aebb30cddd8c
hyeon <vikim1210@naver.com>
2023-08-12 00:11:36 +0900

    [Feat] DMakerController,DMakerService: 검증한 데이터로 객체를 만들고 post 수행

--- Basic/dmaker/src/main/java/com/developer/maker/dmaker/controller/DMakerController.java
+++ Basic/dmaker/src/main/java/com/developer/maker/dmaker/controller/DMakerController.java
@@ -1,6 +1,8 @@
 package com.developer.maker.dmaker.controller;
 
+import com.developer.maker.dmaker.dto.CreateDeveloper;
 import com.developer.maker.dmaker.service.DMakerService;
+import jakarta.validation.Valid;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
@@ -8,6 +10,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
@@ -25,10 +28,12 @@ public class DMakerController {
     }
 
     @PostMapping("/create-developers")
-    public List<String> createDevelopers(){
-        log.info("GET /create-developers HTTP/1.1");
+    public List<String> createDevelopers(
+        @Valid @RequestBody CreateDeveloper.Request request
+    ){
+        log.info("request : {}", request);
 
-        dMakerService.createDeveloper();
+        dMakerService.createDeveloper(request);
 
         return Collections.singletonList("Barbie");
     }
--- Basic/dmaker/src/main/java/com/developer/maker/dmaker/service/DMakerService.java
+++ Basic/dmaker/src/main/java/com/developer/maker/dmaker/service/DMakerService.java
@@ -1,10 +1,13 @@
 package com.developer.maker.dmaker.service;
 
+import com.developer.maker.dmaker.dto.CreateDeveloper;
+import com.developer.maker.dmaker.dto.CreateDeveloper.Request;
 import com.developer.maker.dmaker.entity.Developer;
 import com.developer.maker.dmaker.repository.DeveloperRepository;
 import com.developer.maker.dmaker.type.DeveloperLevel;
 import com.developer.maker.dmaker.type.DeveloperSkillType;
 import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -14,7 +17,7 @@ public class DMakerService {
     private final DeveloperRepository developerRepository;
 
     @Transactional
-    public void createDeveloper(){
+    public void createDeveloper(CreateDeveloper.Request request){
         Developer developer = Developer.builder()
             .developerLevel(DeveloperLevel.JUNIOR)
             .developerSkillType(DeveloperSkillType.BACK_END)
