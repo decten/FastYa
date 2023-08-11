7b44ed41ea3a64fd7fda2a6dfcfcb1b848eefa26
hyeon <vikim1210@naver.com>
2023-08-12 01:55:14 +0900

    [Add&Feat] DeveloperRepository: 옵셔널 활용한 멤버 변수 찾는 함수 추가

--- Basic/dmaker/src/main/java/com/developer/maker/dmaker/repository/DeveloperRepository.java
+++ Basic/dmaker/src/main/java/com/developer/maker/dmaker/repository/DeveloperRepository.java
@@ -1,6 +1,7 @@
 package com.developer.maker.dmaker.repository;
 
 import com.developer.maker.dmaker.entity.Developer;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +9,5 @@ import org.springframework.stereotype.Repository;
 public interface DeveloperRepository
     extends JpaRepository<Developer, Long> {
 
+    Optional<Developer> findByMemberId(String memberId);
 }
