123e5d5d1b5e3670cd4d403d5b896a25a1cf6db8
hyeon <vikim1210@naver.com>
2023-08-12 01:39:51 +0900

    [Refactor] DMakerService: RuntimeException 클래스 대신 커스텀 에러 클래스와 Enum 활용

--- Basic/dmaker/src/main/java/com/developer/maker/dmaker/service/DMakerService.java
+++ Basic/dmaker/src/main/java/com/developer/maker/dmaker/service/DMakerService.java
@@ -1,11 +1,15 @@
 package com.developer.maker.dmaker.service;
 
+import static exception.DMakerErrorCode.LEVEL_EXPERIENCE_YEARS_NOT_MATCHED;
+
 import com.developer.maker.dmaker.dto.CreateDeveloper;
 import com.developer.maker.dmaker.dto.CreateDeveloper.Request;
 import com.developer.maker.dmaker.entity.Developer;
 import com.developer.maker.dmaker.repository.DeveloperRepository;
 import com.developer.maker.dmaker.type.DeveloperLevel;
 import com.developer.maker.dmaker.type.DeveloperSkillType;
+import exception.DMakerErrorCode;
+import exception.DMakerException;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -18,6 +22,9 @@ public class DMakerService {
 
     @Transactional
     public void createDeveloper(CreateDeveloper.Request request){
+        validateCreateDeveloperRequest(request);
+
+        //비즈니스 로직 수행
         Developer developer = Developer.builder()
             .developerLevel(DeveloperLevel.JUNIOR)
             .developerSkillType(DeveloperSkillType.BACK_END)
@@ -27,5 +34,13 @@ public class DMakerService {
             .build();
 
         developerRepository.save(developer);
+    }
+
+    private void validateCreateDeveloperRequest(Request request) {
+        //비즈니스 밸리데이션 수행
+        if(request.getDeveloperLevel() == DeveloperLevel.SENIOR
+        && request.getExperienceYears() < 10){
+            throw new DMakerException(LEVEL_EXPERIENCE_YEARS_NOT_MATCHED);
+        }
     }
 }
