9a3ffba41a037f200ec089d0df325a42099f1ac2
hyun <decten@github.com>
2023-08-14 02:36:29 +0900

    [Modify] DMakerController: getDeveloperDetail 함수의 GetMapping url 수정

--- Basic/dmaker/src/main/java/com/developer/maker/dmaker/controller/DMakerController.java
+++ Basic/dmaker/src/main/java/com/developer/maker/dmaker/controller/DMakerController.java
@@ -25,7 +25,7 @@ public class DMakerController {
         return dMakerService.getAllDevelopers();
     }
 
-    @GetMapping("/developers/{memberId}")
+    @GetMapping("/developer/{memberId}")
     public DeveloperDetailDto getDeveloperDetail(
             @PathVariable String memberId
     ){
