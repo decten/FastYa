d97e4f04edb09dbd3d1adcbe1a7c4029566bf743
hyeon <vikim1210@naver.com>
2023-08-15 23:46:30 +0900

    [Feat] DMakerExceptionHandler: 미리 작성한 에러 클래스에는 없지만, 기본적으로 제공되는 클래스 활용한 사용자 입력 에러 처리

--- Basic/dmaker/src/main/java/com/developer/maker/dmaker/exception/DMakerExceptionHandler.java
+++ Basic/dmaker/src/main/java/com/developer/maker/dmaker/exception/DMakerExceptionHandler.java
@@ -1,9 +1,13 @@
 package com.developer.maker.dmaker.exception;
 
+import static com.developer.maker.dmaker.exception.DMakerErrorCode.INVALID_REQUEST;
+
 import com.developer.maker.dmaker.dto.DMakerErrorResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -24,5 +28,21 @@ public class DMakerExceptionHandler {
             .errorMessage(e.getDetailMessage())
             .build();
     }
+
+    @ExceptionHandler(value = {
+        HttpRequestMethodNotSupportedException.class,
+        MethodArgumentNotValidException.class
+    })
+    public DMakerErrorResponse handleBadRequest(
+        Exception e, HttpServletRequest request
+    ){
+        log.error("url: {}, message: {}", request.getRequestURI(), e.getMessage());
+
+        return DMakerErrorResponse.builder()
+            .errorCode(INVALID_REQUEST)
+            .errorMessage(INVALID_REQUEST.getMessage())
+            .build();
+    }
+
 
 }
